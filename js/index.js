aa292c92e59ec6777b1ba6456e80db6bcbbef184
Nickdem <nickdem559@inbox.ru>
2020-12-01 15:47:02 +0300

    адаптивка

--- js/index.js
+++ js/index.js
@@ -2,20 +2,29 @@ const clickers = document.querySelectorAll('#clicker');
 const exitBtn = document.querySelector('.exit');
 const backDrop = document.querySelector('.backdrop');
 const popup = document.querySelector('.consultation');
+const menuBtn = document.querySelector('.header__menu-toggle');
+const menu = document.querySelector('.menu ul');
+
+console.log(menu)
 
 clickers.forEach(clicker => {
-    clicker.addEventListener('click', (e) => {
-        e.preventDefault()
-        popup.classList.remove('hidden')
-    })
+  clicker.addEventListener('click', (e) => {
+    e.preventDefault()
+    popup.classList.remove('hidden')
+  })
+})
+
+menuBtn.addEventListener('click', () => {
+  menu.style.display = 'block'
+  menuBtn.style.display = 'none'
 })
 
 exitBtn.addEventListener('click', (e) => {
-    e.preventDefault()
-    popup.classList.add('hidden')
+  e.preventDefault()
+  popup.classList.add('hidden')
 })
 
 backDrop.addEventListener('click', (e) => {
-    e.preventDefault()
-    popup.classList.add('hidden')
+  e.preventDefault()
+  popup.classList.add('hidden')
 })
